7d5f0dd1392f090b2c9d659b9568b18463cd9d6d
777Roma777 <nanoeagle911@gmail.com>
2023-08-01 14:41:50 +0300

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
 
+createGallery();
+
+function createGalleryElement(item) {
+  return `<a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+}
+
+function createGallery() {
+  const galleryHtml = galleryItems.map(createGalleryElement).join('');
+  gallery.innerHTML = galleryHtml;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const strigifyData = JSON.stringify(data);
+  localStorage.setItem(CURRENT_TIME, strigifyData);
+};
+player.on('play', throttle(onPlay, 1000));
+
+function playResume() {
+  if (JSON.parse(localStorage.getItem(CURRENT_TIME)) === null) {
+    return;
+  }
+
+  const paused = JSON.parse(localStorage.getItem(CURRENT_TIME))['seconds'];
+  if (paused) {
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+}
+playResume();
